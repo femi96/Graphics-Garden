2f4ff92fd1c8a9f004e69abdcd29cfce37e3265e
Femi Oladipupo <Femi@DESKTOP-D8TQN6P.mit.edu>
2018-11-23 15:59:34 -0500

    Add ParticleSystem with SpringSystem example

---
+++ Assets/Scripts/ParticleSystem/ParticleSystemCustom.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ParticleSystemCustom : MonoBehaviour {
+  /*
+  Particle system abstract class using first order derivatives
+  */
+
+  public Vector3[] state;
+  public bool reset = true;
+
+  void Update() {
+
+    if (reset) {
+      CreateState();
+      reset = false;
+    }
+
+    ResetParticles();
+    StepRungeKutta4(Time.deltaTime);
+    RenderState();
+  }
+
+  /*
+  Creates the state vector and initial render variables
+  */
+  public abstract void CreateState();
+
+  /*
+  Calculate first derivative of given state
+  */
+  public abstract Vector3[] EvalF(Vector3[] evalState);
+
+  /*
+  Render or change rendered state
+  */
+  public abstract void RenderState();
+
+  /*
+  Reset particles if applicable
+  */
+  public abstract void ResetParticles();
+
+  /*
+  Step the system with Runge-Kutta 4 method
+  */
+  public void StepRungeKutta4(float stepSize) {
+    /*
+    RK4 from 6.837 Lecture 8 slides
+
+      k1 = f(xn, tn)
+      k2 = f(xn + h/2 k1, tn + h/2)
+      k3 = f(xn + h/2 k2, tn + h/2)
+      k4 = f(xn + h k3, tn + h)
+
+      xn+1 = xn + h/6 (k1 + 2 k2 + 2 k3 + k4)
+      tn+1 = tn + h
+    */
+
+    int s = state.Length;
+    float h = stepSize;
+
+    // Set k1
+    Vector3[] xn = state;
+    Vector3[] k1 = EvalF(xn);
+
+    Vector3[] xk1 = new Vector3[s];
+    Vector3[] xk2 = new Vector3[s];
+    Vector3[] xk3 = new Vector3[s];
+    Vector3[] xn1 = new Vector3[s];
+
+    // Set xk1 & k2
+    for (int i = 0; i < s; ++i)
+      xk1[i] = xn[i] + (k1[i] * (h / 2f));
+
+    Vector3[] k2 = EvalF(xk1);
+
+    // Set xk2 & k3
+    for (int i = 0; i < s; ++i)
+      xk2[i] = xn[i] + (k2[i] * (h / 2f));
+
+    Vector3[] k3 = EvalF(xk2);
+
+    // Set xk3 & k4
+    for (int i = 0; i < s; ++i)
+      xk3[i] = xn[i] + (k3[i] * h);
+
+    Vector3[] k4 = EvalF(xk3);
+
+    // Set xn+1
+    for (int i = 0; i < s; ++i)
+      xn1[i] = xn[i] + ((k1[i] + (k2[i] * 2f) + (k3[i] * 2f) + k4[i]) * (h / 6f));
+
+    state = xn1;
+  }
+}
---
+++ Assets/Scripts/ParticleSystem/ParticleSystems/SpringSystem.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringSystem : ParticleSystemCustom {
+
+  public int numParticles = 4;
+  public int[] fixedParticles = new int[] {0};
+  private List<int> springPairs;
+
+  private GameObject[] particlesObjs;
+  public GameObject particleObj;
+
+  public const float Gravity = 10.0f;
+
+  public const float ParticleMass = 1.0f;
+  public const float ParticleDrag = 0.1f;
+
+  public const float SpringConstant = 10.0f;
+  public const float SpringDistance = 2.5f;
+
+  public override void CreateState() {
+
+    // Clear child objects
+    foreach (Transform child in transform)
+      GameObject.Destroy(child.gameObject);
+
+
+    // State is (x, v)
+    state = new Vector3[numParticles * 2];
+
+    // x
+    for (int i = 0; i < numParticles; ++i)
+      state[i] = new Vector3(i, 0, 0);
+
+    // v
+    for (int i = numParticles; i < numParticles * 2; ++i)
+      state[i] = new Vector3();
+
+    // Add springs
+    springPairs = new List<int>();
+
+    for (int i = 0; i < numParticles - 1; ++i) {
+      springPairs.Add(i);
+      springPairs.Add(i + 1);
+    }
+
+
+    // Create render objects
+    particlesObjs = new GameObject[numParticles];
+
+    for (int i = 0; i < numParticles; ++i)
+      particlesObjs[i] = Instantiate(particleObj, state[i], Quaternion.identity, transform);
+  }
+
+  public override Vector3[] EvalF(Vector3[] evalState) {
+
+    // Take state which is (x, v)
+    // Output state which is (v, F/M)
+
+
+    // F = -mg -dv -kx
+    Vector3[] force = new Vector3[numParticles];
+
+    // Apply particle forces
+    for (int i = 0; i < numParticles; ++i) {
+      force[i] = new Vector3();
+      force[i] += new Vector3(0, -Gravity * ParticleMass, 0);
+      force[i] += -ParticleDrag * evalState[numParticles + i];
+    }
+
+    // Apply spring forces
+    for (int i = 0; i < springPairs.Count; i += 2) {
+
+      int particleIndex1 = springPairs[i];
+      int particleIndex2 = springPairs[i + 1];
+
+      Vector3 particle1 = evalState[particleIndex1];
+      Vector3 particle2 = evalState[particleIndex2];
+
+      Vector3 p1top2 = particle2 - particle1;
+      Vector3 p2top1 = particle1 - particle2;
+
+      float dist = p1top2.magnitude;
+      float deltaForce = -SpringConstant * (dist - SpringDistance);
+
+      force[particleIndex1] += deltaForce * p2top1 / dist;
+      force[particleIndex2] += deltaForce * p1top2 / dist;
+    }
+
+
+    // Create newState
+    Vector3[] newState = new Vector3[numParticles * 2];
+
+    for (int i = 0; i < numParticles; ++i) {
+      newState[i] = evalState[numParticles + i];
+      newState[i + numParticles] = force[i] / ParticleMass;
+    }
+
+    foreach (int i in fixedParticles) {
+      newState[i] = new Vector3();
+      newState[i + numParticles] = new Vector3();
+    }
+
+    return newState;
+  }
+
+  public override void RenderState() {
+    for (int i = 0; i < numParticles; ++i)
+      particlesObjs[i].transform.position = state[i];
+  }
+
+  public override void ResetParticles() {} // Never resets particles
+}
