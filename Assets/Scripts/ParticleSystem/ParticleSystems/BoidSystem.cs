f17fc8be23746f8aa76a306182cf1e17056af814
Femi Oladipupo <Femi@DESKTOP-D8TQN6P.mit.edu>
2018-11-23 22:36:02 -0500

    Change Boid to use public fields

--- Assets/Scripts/ParticleSystem/ParticleSystems/BoidSystem.cs
+++ Assets/Scripts/ParticleSystem/ParticleSystems/BoidSystem.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class BoidSystem : ParticleSystemCustom {
 
-  public int numParticles = 7;
+  public int numBoids = 20;
 
-  public GameObject particleObj;
-  private GameObject[] particlesObjs;
+  public GameObject boidObj;
+  private GameObject[] boidObjects;
 
-  public const float BoidSpeed = 1.0f;
-  public const float BoidSpeedConstant = 1.0f;
+  [Header("Boid Speed Settings")]
+  public float BoidSpeed = 1.0f;
+  public float BoidSpeedDragConstant = 1.0f;
 
-  public const float FlockMinDistance = 1f;
-  public const float FlockMaxDistance = 2.5f;
+  [Header("Flock Settings")]
+  public float FlockMinDistance = 1f;
+  public float FlockMaxDistance = 2.5f;
 
-  public const float EdgeLimit = 5f;
+  [Header("Boundary Settings")]
+  public float BoundaryDistance = 4f;
 
-  public const float WeightSeparation = 1.0f;
-  public const float WeightAlignment = 1.0f;
-  public const float WeightCohesion = 1.0f;
-  public const float WeightNoise = 0.2f;
-  public const float WeightAvoidance = 1.0f;
+  [Header("Factor Weight Settings")]
+  public float WeightSeparation = 1.0f;
+  public float WeightAlignment = 1.0f;
+  public float WeightCohesion = 1.0f;
+  public float WeightNoise = 0.2f;
+  public float WeightAvoidance = 1.0f;
 
   public override void CreateState() {
 
@@ -31,20 +35,20 @@ public class BoidSystem : ParticleSystemCustom {
 
 
     // State is (x, v)
-    state = new Vector3[numParticles * 2];
+    state = new Vector3[numBoids * 2];
 
-    for (int i = 0; i < numParticles; ++i)
+    for (int i = 0; i < numBoids; ++i)
       state[i] = Random.onUnitSphere * Random.Range(-5, 5); // x
 
-    for (int i = numParticles; i < numParticles * 2; ++i)
+    for (int i = numBoids; i < numBoids * 2; ++i)
       state[i] = Random.onUnitSphere; // v
 
 
     // Create render objects
-    particlesObjs = new GameObject[numParticles];
+    boidObjects = new GameObject[numBoids];
 
-    for (int i = 0; i < numParticles; ++i)
-      particlesObjs[i] = Instantiate(particleObj, state[i], Quaternion.identity, transform);
+    for (int i = 0; i < numBoids; ++i)
+      boidObjects[i] = Instantiate(boidObj, state[i], Quaternion.identity, transform);
   }
 
   public override Vector3[] EvalF(Vector3[] evalState) {
@@ -54,17 +58,17 @@ public class BoidSystem : ParticleSystemCustom {
 
 
     // A = steer + speed control
-    Vector3[] accel = new Vector3[numParticles];
+    Vector3[] accel = new Vector3[numBoids];
 
-    for (int i = 0; i < numParticles; ++i) {
+    for (int i = 0; i < numBoids; ++i) {
       accel[i] = new Vector3();
       Vector3 boidPos = evalState[i];
-      Vector3 boidVel = evalState[numParticles + i];
+      Vector3 boidVel = evalState[numBoids + i];
 
       // Steer = separation + alignment + cohesion
       List<int> flockIndices = new List<int>();
 
-      for (int j = 0; j < numParticles; ++j) {
+      for (int j = 0; j < numBoids; ++j) {
         Vector3 frenPos = evalState[j];
         Vector3 deltaPos = boidPos - frenPos;
 
@@ -75,7 +79,7 @@ public class BoidSystem : ParticleSystemCustom {
         if (i == j) continue;
 
         Vector3 frenPos = evalState[j];
-        Vector3 frenVel = evalState[numParticles + j];
+        Vector3 frenVel = evalState[numBoids + j];
         Vector3 deltaPos = boidPos - frenPos;
 
         // Separation
@@ -89,30 +93,30 @@ public class BoidSystem : ParticleSystemCustom {
       // Noise
       accel[i] += Random.onUnitSphere * Random.Range(0, WeightNoise);
       // Limit
-      accel[i] += WeightAvoidance * -boidPos.normalized * Mathf.Max(0, boidPos.magnitude - EdgeLimit);
+      accel[i] += WeightAvoidance * -boidPos.normalized * Mathf.Max(0, boidPos.magnitude - BoundaryDistance);
 
       // Speed control
-      accel[i] += BoidSpeedConstant * (BoidSpeed - boidVel.magnitude) * boidVel.normalized;
+      accel[i] += BoidSpeedDragConstant * (BoidSpeed - boidVel.magnitude) * boidVel.normalized;
     }
 
 
     // Create newState
-    Vector3[] newState = new Vector3[numParticles * 2];
+    Vector3[] newState = new Vector3[numBoids * 2];
 
-    for (int i = 0; i < numParticles; ++i) {
-      newState[i] = evalState[numParticles + i];
-      newState[i + numParticles] = accel[i];
+    for (int i = 0; i < numBoids; ++i) {
+      newState[i] = evalState[numBoids + i];
+      newState[i + numBoids] = accel[i];
     }
 
     return newState;
   }
 
   public override void RenderState() {
-    for (int i = 0; i < numParticles; ++i) {
-      particlesObjs[i].transform.position = state[i];
+    for (int i = 0; i < numBoids; ++i) {
+      boidObjects[i].transform.position = state[i];
 
-      if (state[numParticles + i].magnitude > 0.0001f)
-        particlesObjs[i].transform.rotation =  Quaternion.LookRotation(state[numParticles + i], Vector3.up);
+      if (state[numBoids + i].magnitude > 0.0001f)
+        boidObjects[i].transform.rotation =  Quaternion.LookRotation(state[numBoids + i], Vector3.up);
     }
   }
 
