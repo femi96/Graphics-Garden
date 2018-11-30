34c13e012868d3138a7060543e0bf54bd9b6e738
Femi Oladipupo <Femi@DESKTOP-D8TQN6P.mit.edu>
2018-11-30 00:32:34 -0500

    Add MeshGenerator with FractalBinaryTreeMesh example

---
+++ Assets/Scripts/LindenmayerSystem/MeshGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MeshGenerator : SystemGenerator {
+  /*
+  Subclass of SystemGenerators that generate meshes
+  */
+
+  private Mesh mesh;
+  private List<Vector3> vertices;
+  private List<int> triangles;
+
+  public void CreateMesh() {
+    mesh = new Mesh();
+    vertices = new List<Vector3>();
+    triangles = new List<int>();
+    GetComponent<MeshFilter>().mesh = mesh;
+  }
+
+  public void AddSegment(Vector3 p1, Vector3 p2, float w1, float w2) {
+    Vector3 p1t2 = p2 - p1;
+    Quaternion zRotation = Quaternion.LookRotation(p1t2, Vector3.up);
+
+    int z1 = vertices.Count;
+
+    for (int i = 0; i < 4; i++) {
+      float rad = 2f * Mathf.PI * i / 4;
+      Vector3 v = new Vector3(Mathf.Sin(rad), Mathf.Cos(rad), 0f);
+      vertices.Add(p1 + (zRotation * v).normalized * w1);
+    }
+
+    int z2 = vertices.Count;
+
+    for (int i = 0; i < 4; i++) {
+      float rad = 2f * Mathf.PI * i / 4;
+      Vector3 v = new Vector3(Mathf.Sin(rad), Mathf.Cos(rad), 0f);
+      vertices.Add(p2 + (zRotation * v).normalized * w2);
+    }
+
+    triangles.Add(z1 + 0);
+    triangles.Add(z2 + 0);
+    triangles.Add(z1 + 1);
+    triangles.Add(z2 + 0);
+    triangles.Add(z2 + 1);
+    triangles.Add(z1 + 1);
+
+    triangles.Add(z1 + 1);
+    triangles.Add(z2 + 1);
+    triangles.Add(z1 + 2);
+    triangles.Add(z2 + 1);
+    triangles.Add(z2 + 2);
+    triangles.Add(z1 + 2);
+
+    triangles.Add(z1 + 2);
+    triangles.Add(z2 + 2);
+    triangles.Add(z1 + 3);
+    triangles.Add(z2 + 2);
+    triangles.Add(z2 + 3);
+    triangles.Add(z1 + 3);
+
+    triangles.Add(z1 + 3);
+    triangles.Add(z2 + 3);
+    triangles.Add(z1 + 0);
+    triangles.Add(z2 + 3);
+    triangles.Add(z2 + 0);
+    triangles.Add(z1 + 0);
+  }
+
+  public void UpdateMesh() {
+    mesh.vertices = vertices.ToArray();
+    mesh.triangles = triangles.ToArray();
+    mesh.RecalculateNormals();
+  }
+}
---
+++ Assets/Scripts/LindenmayerSystem/MeshGenerators/FractalBinaryTreeMesh.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractalBinaryTreeMesh : MeshGenerator {
+
+  public override void CreateSystem() {
+    lSystem = new LindenmayerSystem("0");
+    lSystem.AddRule('[', new LTerminal());
+    lSystem.AddRule(']', new LTerminal());
+    lSystem.AddRule('0', new LVariable("1[0]0"));
+    lSystem.AddRule('1', new LVariable("11"));
+  }
+
+  // Game objects
+  public GameObject segment;
+
+  // LIFO save stack placement
+  private Vector3 currentPos;
+  private Quaternion currentRot;
+
+  private List<Vector3> positionStack;
+  private List<Quaternion> rotationStack;
+
+  public override void GenerateFromState() {
+    CreateMesh();
+
+    foreach (Transform child in transform) {
+      GameObject.Destroy(child.gameObject);
+    }
+
+    string state = lSystem.GetState();
+    currentPos = new Vector3();
+    currentRot = Quaternion.identity;
+
+    positionStack = new List<Vector3>() { currentPos };
+    rotationStack = new List<Quaternion>() { currentRot };
+
+    foreach (char c in state) {
+      int stackEnd = positionStack.Count - 1;
+
+      switch (c) {
+
+      case '0':
+        // Draw line segment with leaf
+        AddSegment(currentPos, currentPos + currentRot * Vector3.up, 0.25f, 0.25f);
+        currentPos += currentRot * Vector3.up;
+        AddSegment(currentPos, currentPos + (currentRot * Vector3.up) / 3f, 0.25f, 0.0f);
+        break;
+
+      case '1':
+        // Draw line segment
+        AddSegment(currentPos, currentPos + currentRot * Vector3.up, 0.25f, 0.25f);
+        currentPos += currentRot * Vector3.up;
+        break;
+
+      case '[':
+        // Save placement, rotate 45deg
+        positionStack.Add(currentPos);
+        rotationStack.Add(currentRot);
+        currentRot *= Quaternion.AngleAxis(45, Vector3.right);
+        break;
+
+      case ']':
+        // Load placement, rotate -45deg
+        currentPos = positionStack[stackEnd];
+        currentRot = rotationStack[stackEnd];
+        positionStack.RemoveAt(stackEnd);
+        rotationStack.RemoveAt(stackEnd);
+        currentRot *= Quaternion.AngleAxis(-45, Vector3.right);
+        break;
+      }
+    }
+
+    UpdateMesh();
+  }
+}
