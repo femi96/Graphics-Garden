cf40b51075836b27b0ad4cd2eeee73ff00f54974
Femi Oladipupo <femi@femioladipupo-mbpr.attlocal.net>
2019-01-19 17:00:40 -0800

    Add perlin noise and chunk random generation

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour {
 
-  private int ChunkSize = 11;
-  private float NodeDistance = 1f;
+  public static int NumNodes = 11;
+  public static float Size = 10f;
+  private float nodeDistance;
 
   private MeshFilter mf;
   private MeshRenderer mr;
@@ -17,13 +18,15 @@ public class Chunk : MonoBehaviour {
   private float[,] humids;
 
   void Start() {
+    nodeDistance = Size / (NumNodes - 1);
+
     mf = GetComponent<MeshFilter>();
     // mr = GetComponent<MeshRenderer>();
     world = GameObject.Find("World").GetComponent<World>();
 
-    heights = new float[ChunkSize, ChunkSize];
-    temps = new float[ChunkSize, ChunkSize];
-    humids = new float[ChunkSize, ChunkSize];
+    heights = new float[NumNodes, NumNodes];
+    temps = new float[NumNodes, NumNodes];
+    humids = new float[NumNodes, NumNodes];
 
     UpdateMesh();
   }
@@ -32,9 +35,9 @@ public class Chunk : MonoBehaviour {
     Vector3 baseV = transform.position;
 
     // Fill values
-    for (int i = 0; i < ChunkSize; i++) {
-      for (int j = 0; j < ChunkSize; j++) {
-        Vector3 v = baseV + new Vector3(i * NodeDistance, 0, j * NodeDistance);
+    for (int i = 0; i < NumNodes; i++) {
+      for (int j = 0; j < NumNodes; j++) {
+        Vector3 v = baseV + new Vector3(i * nodeDistance, 0, j * nodeDistance);
         heights[i, j] = world.GetHeight(v);
         temps[i, j] = world.GetTemperature(v);
         humids[i, j] = world.GetHumidity(v);
@@ -44,37 +47,85 @@ public class Chunk : MonoBehaviour {
     // Fill mesh
     Mesh mesh = new Mesh();
 
-    Vector3[] newVertices = new Vector3[ChunkSize * ChunkSize];
-    Vector2[] newUV = new Vector2[ChunkSize * ChunkSize];
+    List<Vector3> newVertices = new List<Vector3>();
+    List<Vector2> newUV = new List<Vector2>();
     List<int> newTriangles = new List<int>();
 
-    for (int i = 0; i < ChunkSize; i++) {
-      for (int j = 0; j < ChunkSize; j++) {
-        int k = i + j * ChunkSize;
-        Vector3 v = new Vector3(i * NodeDistance, heights[i, j], j * NodeDistance);
-        newVertices[k] = v;
-        newUV[k] = new Vector2(0, 0);
+    if (!world.smoothChunk) {
+      for (int i = 0; i < NumNodes - 1; i++) {
+        for (int j = 0; j < NumNodes - 1; j++) {
+          Vector3 v00 = new Vector3((i + 0) * nodeDistance, heights[i + 0, j + 0], (j + 0) * nodeDistance);
+          Vector3 v01 = new Vector3((i + 0) * nodeDistance, heights[i + 0, j + 1], (j + 1) * nodeDistance);
+          Vector3 v10 = new Vector3((i + 1) * nodeDistance, heights[i + 1, j + 0], (j + 0) * nodeDistance);
+          Vector3 v11 = new Vector3((i + 1) * nodeDistance, heights[i + 1, j + 1], (j + 1) * nodeDistance);
+          Vector3 vc_ = (v00 + v01 + v10 + v11) * 0.25f;
+
+          int baseK = newVertices.Count;
+          int k00 = baseK + 0;
+          int k01 = baseK + 1;
+          int k10 = baseK + 2;
+          int k11 = baseK + 3;
+          int kc_ = baseK + 4;
+
+          newVertices.Add(v00);
+          newVertices.Add(v01);
+          newVertices.Add(v10);
+          newVertices.Add(v11);
+          newVertices.Add(vc_);
+
+          newUV.Add(new Vector2(0, 0));
+          newUV.Add(new Vector2(0, 0));
+          newUV.Add(new Vector2(0, 0));
+          newUV.Add(new Vector2(0, 0));
+          newUV.Add(new Vector2(0, 0));
+
+          newTriangles.Add(k00);
+          newTriangles.Add(k01);
+          newTriangles.Add(kc_);
+
+          newTriangles.Add(k01);
+          newTriangles.Add(k11);
+          newTriangles.Add(kc_);
+
+          newTriangles.Add(k11);
+          newTriangles.Add(k10);
+          newTriangles.Add(kc_);
+
+          newTriangles.Add(k10);
+          newTriangles.Add(k00);
+          newTriangles.Add(kc_);
+        }
+      }
+    } else {
+
+
+      for (int j = 0; j < NumNodes; j++) {
+        for (int i = 0; i < NumNodes; i++) {
+          Vector3 v = new Vector3(i * nodeDistance, heights[i, j], j * nodeDistance);
+          newVertices.Add(v);
+          newUV.Add(new Vector2(0, 0));
+        }
+      }
+
+      for (int i = 0; i < NumNodes - 1; i++) {
+        for (int j = 0; j < NumNodes - 1; j++) {
+          int k00 = (i + 0) + (j + 0) * NumNodes;
+          int k01 = (i + 0) + (j + 1) * NumNodes;
+          int k10 = (i + 1) + (j + 0) * NumNodes;
+          int k11 = (i + 1) + (j + 1) * NumNodes;
+          newTriangles.Add(k00);
+          newTriangles.Add(k01);
+          newTriangles.Add(k10);
+          newTriangles.Add(k10);
+          newTriangles.Add(k01);
+          newTriangles.Add(k11);
+        }
       }
     }
 
-    for (int i = 0; i < ChunkSize - 1; i++) {
-      for (int j = 0; j < ChunkSize - 1; j++) {
-        int k00 = (i + 0) + (j + 0) * ChunkSize;
-        int k01 = (i + 0) + (j + 1) * ChunkSize;
-        int k10 = (i + 1) + (j + 0) * ChunkSize;
-        int k11 = (i + 1) + (j + 1) * ChunkSize;
-        newTriangles.Add(k00);
-        newTriangles.Add(k01);
-        newTriangles.Add(k10);
-        newTriangles.Add(k10);
-        newTriangles.Add(k01);
-        newTriangles.Add(k11);
-      }
-    }
-
-    mesh.vertices = newVertices;
-    mesh.uv = newUV;
+    mesh.vertices = newVertices.ToArray();
     mesh.triangles = newTriangles.ToArray();
+    mesh.uv = newUV.ToArray();
     mesh.RecalculateNormals();
 
     mf.mesh = mesh;
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -7,24 +7,38 @@ public class World : MonoBehaviour {
   public GameObject loadTarget;
   public GameObject chunkPrefab;
 
+  public bool reset = true;
+  public bool smoothChunk = false;
+
   void Start() {
-    LoadBlockOfChunks();
+
   }
 
   void Update() {
     // Load correct chunks
+
+    // Currently just a constant block
+    if (reset) {
+      reset = false;
+      LoadBlockOfChunks();
+    }
   }
 
   private void LoadBlockOfChunks() {
+    foreach (Transform child in transform)
+      Destroy(child.gameObject);
+
     for (int i = -2; i < 3; i++) {
       for (int j = -2; j < 3; j++) {
-        Instantiate(chunkPrefab, new Vector3(i * 10f, 0, j * 10f), Quaternion.identity, transform);
+        Instantiate(chunkPrefab, new Vector3(i * Chunk.Size, 0, j * Chunk.Size), Quaternion.identity, transform);
       }
     }
   }
 
   public float GetHeight(Vector3 v) {
-    return v.magnitude;
+    v = v - Vector3.up * v.y;
+    return 8f * (Perlin.Noise(v * 0.05f) + 0.5f);
+    // return v.magnitude;
   }
 
   public float GetTemperature(Vector3 v) {
